ca3df305c1f5230d53936b0b0e4cd5938c55a3b2
george-qiaozhi <george.qiaozhi@gmail.com>
2017-10-23 11:35:40 -0500

    add comments

--- merge.py
+++ merge.py
@@ -2,24 +2,31 @@ import csv, os
 import pandas as pd 
 
 def main():
-    # configure the source and target file, and the fields to merge
-    #labs = ["301", "302", "303", "304", "305", "306", "307", "308", "309", "310", "311", "312", "314", "315"]
+    # read subfolder for labs and main sections source .csv file into lists
     labs = os.listdir("labSection/")
-    #lectures = ["001", "002"]
+    print(labs)
     lectures = os.listdir("mainSection/")
-    common_key = ["Username"] # Username is used to match student's record between lab and main section
-    #avilable = ["Availability"]  # if Availability is No, skip this record
+    print(lectures)
+
+    # Username is used to match student's record between lab and main section
+    common_key = ["Username"] 
+
+    # avilable = ["Availability"]  # if Availability is No, skip this record
+    
+    # the column need to transfer from lab section to main section
     columns_to_transfer = ["Lab 01", "Lab 02", "Lab 03", "Lab 04", "Lab 05", "Lab 06", "Lab 07", "Lab 08", "Lab 09", "Lab 10", "Lab 11", "Lab 12",
      "Quiz 01", "Quiz 02", "Quiz 03", "Quiz 04", "Quiz 05", "Quiz 06", "Quiz 07", "Quiz 08", "Quiz 09", "Quiz 10", 
      "Lab Exam 01", "Lab Exam 02", "Lab Final Exam"]
-    # columns_to_transfer = ["Lab 01", "Lab 02"]
-    # labs = ["301", "302", "303", "304"]
+
+    # working_fields is used to create intermidiate tables
     working_fields = common_key + columns_to_transfer
     
-    #format verification
+    # format verification
     CONT = True
     for lab in labs:
+    	# parse the input file header for formatting check
         header = list(pd.read_csv(os.path.join("labSection", lab)))
+        # make sure input file have all the columns in columns_to_transfer  
         for field in working_fields:
             PASS = False
             for item in header:
@@ -27,9 +34,11 @@ def main():
                     PASS = True 
             if PASS == False:
                 CONT = False
-                print("INVALID format at " + lab + " Expecting \'" + field + "...")
+                print("INVALID format at " + lab + ". Expecting \'" + field + "...")
     for lec in lectures:
+    	# parse the input file header for formatting check
         header = list(pd.read_csv(os.path.join("mainSection", lec)))
+        # make sure input file have all the columns in columns_to_transfer 
         for field in working_fields:
             PASS = False
             for item in header:
@@ -37,14 +46,18 @@ def main():
                     PASS = True 
             if PASS == False:
                 CONT = False
-                print("INVALID format at " + lec + " Expecting \'" + field + "...")
+                print("INVALID format at " + lec + ". Expecting \'" + field + "...")
 
     # import source files and merge them into a single table workplace
     if(CONT):
+    	# create the intermidiate file aka workplace
         master_t = pd.DataFrame(columns=working_fields)
         for lab in labs:
-            tmp_orgnized = pd.DataFrame(columns=working_fields)# orgnized temp file with column name from working_fields
-            tmp_source = pd.read_csv(os.path.join("labSection", lab))# original file is blackboard's csv file
+        	# orgnized input file columns to the same order as specified in 'columns_to_transfer'.
+        	# each input file might have different column orders, since different TA might have 
+        	# different preference to create columns. But master_t must have one unique order.
+            tmp_orgnized = pd.DataFrame(columns=working_fields)
+            tmp_source = pd.read_csv(os.path.join("labSection", lab))
             for column in tmp_source:
                 for f in working_fields:
                     if f in column:
@@ -52,16 +65,14 @@ def main():
             master_t = master_t.append(tmp_orgnized, ignore_index=True)
         
         for lec in lectures:
-            # import target file
             target_t = pd.read_csv(os.path.join("mainSection", lec))
             
             # print dropped student
-            #print(target_t[~(target_t['Availability'] == 'Yes')])
+            # print(target_t[~(target_t['Availability'] == 'Yes')])
 
-            #mearge the labs to mains
-            #create left table that only have Username
+            # make temp file that merge the labs to mains use left join, table size master_t > target_t
+            # create left table that following main section's Username order
             tmp_target_t_left = target_t.loc[:, ['Username']]
-            #print(master_t)
             tmp_target_t = pd.merge(tmp_target_t_left, master_t, how='left', on=['Username'])
 
             #copy grades to main
@@ -71,9 +82,11 @@ def main():
                 for j in target_t_header:
                     if i in j:
                         target_t[j] = tmp_target_t[i]
-            #save final result to file
-            ## lec[:-4] trim off .csv before adding _ready.csv
+                        
+            # save final result to file
+            # lec[:-4] trim off ".csv" before adding "_ready.csv"
             target_t.to_csv(lec[:-4] + '_ready.csv', index=False, quoting=csv.QUOTE_ALL)
+        print("DONE...")
     else:
         print("ERROR: won't update anything until error fixed...")
 
